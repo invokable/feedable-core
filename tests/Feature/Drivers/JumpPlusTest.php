5ad0d29f97a10d2f93c5cdf9361d653c77b02be7
kawax <kawaxbiz@gmail.com>
2026-01-11 11:07:22 +0900

    Create JumpPlusTest.php

---
+++ tests/Feature/Drivers/JumpPlusTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+it('returns json feed', function (): void {
+    $response = $this->getJson('/shonenjumpplus/daily.json');
+
+    $response->assertOk()
+        ->assertJsonStructure([
+            'version',
+            'title',
+            'home_page_url',
+            'feed_url',
+            'description',
+            'items' => [
+                [
+                    'id',
+                    'url',
+                    'title',
+                    'summary',
+                    'date_published',
+                    'image',
+                    'authors',
+                ],
+            ],
+        ]);
+});
