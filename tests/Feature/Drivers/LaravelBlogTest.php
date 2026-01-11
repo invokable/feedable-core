844424a07def55b5ccf5a06e7c0396f89d457ecb
kawax <kawaxbiz@gmail.com>
2026-01-11 11:24:56 +0900

    Create LaravelBlogTest.php

---
+++ tests/Feature/Drivers/LaravelBlogTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+it('returns json feed', function (): void {
+    $response = $this->getJson('/laravel/blog.json');
+
+    $response->assertOk()
+        ->assertJsonStructure([
+            'version',
+            'title',
+            'home_page_url',
+            'feed_url',
+            'description',
+            'items' => [
+                [
+                    'id',
+                    'url',
+                    'title',
+                    'summary',
+                    'date_published',
+                ],
+            ],
+        ]);
+});
