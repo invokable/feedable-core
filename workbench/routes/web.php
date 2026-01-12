f0ea805b50b452514b44187e84669d1e9e1ca012
kawax <kawaxbiz@gmail.com>
2026-01-12 20:10:40 +0900

    Update web.php

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Revolution\Feedable\Core\Driver;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response(Driver::toPrettyJson())
+        ->header('Content-Type', 'application/json');
 });
