c842c9e4fe022090edee7ad50d86b1bc618308f9
kawax <kawaxbiz@gmail.com>
2026-03-21 10:02:21 +0900

    Update MirrorTest.php

--- tests/Feature/Drivers/MirrorTest.php
+++ tests/Feature/Drivers/MirrorTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 it('returns rss', function (): void {
-    $response = $this->getJson('/mirror?rss=https://feedable-rss.vercel.app/nintendo/direct');
+    $response = $this->getJson('/mirror?rss=https://feedable-rss.vercel.app/laravel/blog');
 
     $response->assertOk()
         ->assertSee('<rss', escape: false);
